4a7e441d61c1bea5faa0c85f482960b3b493f7cd
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-03 18:54:11 -0500

    Buenas practicas de flutter

--- uiflutter/lib/main.dart
+++ uiflutter/lib/main.dart
@@ -6,9 +6,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: Center(
-          child: Text('Hola'),
-        ));
+        home: columnCustom());
+  }
+}
+
+class columnCustom extends StatelessWidget {
+  const columnCustom({
+    Key? key,
+  }) : super(key: key); //Hace referencia al Widget.
+
+  @override
+  Widget build(BuildContext context) {
+    //El build realiza las tareas de redibujado.
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text('data1'), Text('data2')],
+    );
   }
 }
