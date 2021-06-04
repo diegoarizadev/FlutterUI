3f11f256118a29d36eec93001302fcd362614323
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-03 23:35:31 -0500

    Implementacion de gradiantes, pageView con scroll Vertical y 2 pantallas o view

--- uiflutter/lib/screens/scroll_desing.dart
+++ uiflutter/lib/screens/scroll_desing.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
 class ScrollDesignScreen extends StatelessWidget {
+  final boxDecoration = BoxDecoration(
+      gradient: LinearGradient(
+          begin: Alignment.topCenter, //direccion del gradiante
+          end: Alignment.bottomCenter,
+          stops: [0.5, 0.5], //Puntos de quiebre o mitad de la pantalla
+          colors: [Color(0xff5ee8c5), Color(0xff33BAD6)]));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
+        //backgroundColor: Color(0xff33BAD6), //Se agrega color en el fondo para evitar ver el blanco del bounces
+        body: Container(
+      decoration: boxDecoration,
+      child: PageView(
+        //Por defectos, esete componente, tiene Scroll  Horizontal
+        scrollDirection: Axis.vertical, //Cambia la direccion del scroll
+        physics: BouncingScrollPhysics(), //Efecto de rebote de IOS en Android.
+        children: [PageOne(), PageTwo()],
+      ),
+    ));
+  }
+}
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       //Permite colocar Widget, uno encima del otro.
       children: [Background(), MainContent()],
-    ));
+    );
   }
 }
 
@@ -57,6 +79,30 @@ class Background extends StatelessWidget {
       alignment: Alignment.topCenter, //ubica la imagen en la parte superior.
       child: Image(
         image: AssetImage('assets/scroll.png'),
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff33BAD6),
+      child: Center(
+        child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Color(0xff0098FA),
+              shape: StadiumBorder(),
+            ),
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                'Bienvenido',
+                style: TextStyle(color: Colors.white, fontSize: 29.0),
+              ),
+            )),
       ),
     );
   }
