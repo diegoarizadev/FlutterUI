d70b1d1b0eb2ee255a569745660f48a2a5685983
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-23 09:58:29 -0500

    Bordes redondeados.

--- custompainter/lib/src/widgets/headers_widget.dart
+++ custompainter/lib/src/widgets/headers_widget.dart
@@ -11,3 +11,21 @@ class HeaderSqare extends StatelessWidget {
     );
   }
 }
+
+class HeadersRoundedEdges extends StatelessWidget {
+  const HeadersRoundedEdges({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+    );
+  }
+}
