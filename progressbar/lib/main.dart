ee133960bb7f94f832d4ffa31c96341cebf64546
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-24 17:08:04 -0500

    Proyecto base para la creación de un ProgressBar circular con Animated Custom Painter

---
+++ progressbar/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:custompainter/src/reto/cuadrado_animado.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: CuadradoAnimadoPage(),
+    );
+  }
+}
