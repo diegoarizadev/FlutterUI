9f19f9658a4429a985d017bb49f89a8a8a6df884
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-28 15:28:05 -0500

    Initial base for slideShow with  Container and svg

--- slideshow/lib/src/pages/slideshow_pages.dart
+++ slideshow/lib/src/pages/slideshow_pages.dart
@@ -10,9 +10,38 @@ class _SlideShowPageState extends State<SlideShowPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('assets/svg/s6.svg'),
+      body: Center(child: _Slides()),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/svg/s1.svg'),
+          _Slide('assets/svg/s2.svg'),
+          _Slide('assets/svg/s3.svg'),
+        ],
       ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+
+  const _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
