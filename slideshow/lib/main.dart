5a7c2a5c9dd1568e52224d5a5cb285774a6a1e4d
Alejandro Ariza <diego.ariza2@gmail.com>
2021-06-29 17:08:06 -0500

    update

--- slideshow/lib/main.dart
+++ slideshow/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:custompainter/src/pages/piecharts.dart';
 import 'package:custompainter/src/pages/slideshow_pages.dart';
 
 import 'package:flutter/material.dart';
--- slideshow/lib/src/models/slider_model.dart
+++ slideshow/lib/src/models/slider_model.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class SliderModel with ChangeNotifier 0{
+class SliderModel with ChangeNotifier {
   double _currentPage = 0;
 
   double get currentPage => this._currentPage;
 
   set currentPage(double currentePage) {
     this.currentPage = currentePage;
-    notifyListeners();//Notiuficara el cambio a todos los widgets que esten escuchando.
+    notifyListeners(); //Notiuficara el cambio a todos los widgets que esten escuchando.
   }
 }
