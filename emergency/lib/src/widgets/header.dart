18e01a6ec01c90ca969ffdb2822dd8e1a1a31015
Alejandro Ariza <diego.ariza2@gmail.com>
2021-07-08 15:33:14 -0500

    Implementing header custom with FontAwesome

--- emergency/lib/src/widgets/header.dart
+++ emergency/lib/src/widgets/header.dart
@@ -1,6 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconHeader extends StatelessWidget {
+  final colorWhite = Colors.white.withOpacity(0.7);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: [
+            SizedBox(
+              height: 80,
+              width: double
+                  .infinity, //Este widget se expande al ancho del dispositivo lo cual centra todos los elementos.
+            ),
+            Text(
+              'Haz solicitado',
+              style: TextStyle(fontSize: 20, color: colorWhite),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Asistencia Medica',
+              style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+                color: colorWhite,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80,
+              color: Colors.white,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  const _IconHeaderBackground({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
