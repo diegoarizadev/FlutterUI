db6df92e08654a64f567f1048586547d15a59fa2
Alejandro Ariza <diego.ariza2@gmail.com>
2021-07-07 22:34:21 -0500

    Initial implementation of the pinterest type menu.

--- pinterest/lib/src/pages/pinterest.dart
+++ pinterest/lib/src/pages/pinterest.dart
@@ -1,3 +1,4 @@
+import 'package:custompainter/src/widgets/pinterest_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -5,7 +6,8 @@ class PinterestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: PinterestGrid()),
+      body: PinteresMenu(),
+      //body: PinterestGrid(),
     );
   }
 }
--- pinterest/lib/src/widgets/pinterest_menu.dart
+++ pinterest/lib/src/widgets/pinterest_menu.dart
@@ -15,3 +15,35 @@ class PinterestButton {
 
   PinterestButton({required this.onPressed, required this.Icon});
 }
+
+class PinteresMenu extends StatelessWidget {
+  final List<PinterestButton> items = [
+    PinterestButton(
+        onPressed: () {
+          print('Icons.pie_chart');
+        },
+        Icon: Icons.pie_chart),
+    PinterestButton(
+        onPressed: () {
+          print('Icons.search');
+        },
+        Icon: Icons.search),
+    PinterestButton(
+        onPressed: () {
+          print('Icons.notifications');
+        },
+        Icon: Icons.notifications),
+    PinterestButton(
+        onPressed: () {
+          print('Icons.supervised_user_circle');
+        },
+        Icon: Icons.supervised_user_circle)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: Text('Hola desde el menu'),
+    ));
+  }
+}
