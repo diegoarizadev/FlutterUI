477848f2a641e4f57144bbe093adfa11778a2b57
Alejandro Ariza <diego.ariza2@gmail.com>
2021-07-08 15:58:48 -0500

    Class for buttons or menu options handling

--- emergency/lib/src/pages/emergency_page.dart
+++ emergency/lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:emergency/src/widgets/buttonCustom.dart';
 import 'package:emergency/src/widgets/header.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,12 +7,23 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: IconHeader(
+      //body: PageHeader(),
+      body: Center(
+        child: BottonCuston(),
+      ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
       icon: FontAwesomeIcons.plusCircle,
       tittle: 'Haz solicitado.',
       subTittle: 'Asistencia Medica.',
       color0ne: Color(0xff526BF6),
       colorTw0: Color(0xff67ACF2),
-    ));
+    );
   }
 }
---
+++ emergency/lib/src/widgets/buttonCustom.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BottonCuston extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _BottonCustomBackground();
+  }
+}
+
+class _BottonCustomBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      margin: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+//          color: Colors.red,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            offset: Offset(4, 6),
+            blurRadius: 10,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: <Color>[
+            Color(0xff6989f5),
+            Color(0xff906ef5),
+          ],
+        ),
+      ),
+    );
+  }
+}
