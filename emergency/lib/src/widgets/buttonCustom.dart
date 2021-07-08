42ee7316b8808bdf3d146fd34a7fd1b0a5f53bdd
Alejandro Ariza <diego.ariza2@gmail.com>
2021-07-08 16:18:41 -0500

    Customization the icons and text for bottonCustom

--- emergency/lib/src/widgets/buttonCustom.dart
+++ emergency/lib/src/widgets/buttonCustom.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BottonCuston extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _BottonCustomBackground();
+    return Stack(
+      children: [
+        _BottonCustomBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 140,
+              width: 40,
+            ),
+            FaIcon(
+              FontAwesomeIcons.carCrash,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Text(
+                'Accidente de tráfico',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 40,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
 
@@ -11,6 +49,23 @@ class _BottonCustomBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        //Sirve para cortar la imagen, ya que esta se sale del contenedor.
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+              right: -20,
+              top: -20,
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                size: 150,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ],
+        ),
+      ),
       width: double.infinity,
       height: 100,
       margin: EdgeInsets.all(20),
