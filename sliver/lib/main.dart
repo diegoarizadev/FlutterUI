19d1dc67523ac644a61bac3c315eade00113496f
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-07-09 12:24:32 -0500

    Desing Header and Body (ListView) the app

--- sliver/lib/main.dart
+++ sliver/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: SliverPage(),
+      home: SliverListPage(),
     );
   }
 }
--- sliver/lib/src/pages/sliver.dart
+++ sliver/lib/src/pages/sliver.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
 
-class SliverPage extends StatelessWidget {
+class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      body: _HeaderTittle(), //_ListTasks(),
+    );
+  }
+}
+
+class _ListTasks extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) => _ListItem());
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.all(30),
+      child: Text(
+        'Naranjas',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
       ),
+      height: 130,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        borderRadius: BorderRadius.circular(30),
+      ),
+    );
+  }
+}
+
+class _HeaderTittle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            'Nuevo',
+            style: TextStyle(
+              color: Color(0xff532128),
+              fontSize: 50,
+            ),
+          ),
+        ),
+        Stack(
+          children: [
+            SizedBox(
+              width: 100,
+            ),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150,
+                height: 8,
+                color: Color(0xfff7cdd5),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Lista',
+                style: TextStyle(
+                  color: Color(0xffd93a30),
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
