65e50e71a43a94d7fbacaa7ddbea3b60d4bc8dc7
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-07-10 21:40:16 -0500

    initial settings

---
+++ animate/lib/src/pages/pages0ne.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Page0ne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hola Mundo page0n3'),
+    );
+  }
+}
