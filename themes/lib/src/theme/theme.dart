4b2d67c5c212f9a2788b3131e4cfa93e88eb139d
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-07-23 11:04:53 -0500

    Dark and light theme

--- themes/lib/src/theme/theme.dart
+++ themes/lib/src/theme/theme.dart
@@ -4,19 +4,55 @@ import 'package:flutter/material.dart';
 class ThemeChanger with ChangeNotifier {
   bool _darkTheme = false;
   bool _customTheme = false;
+  late ThemeData _currentTheme;
 
   bool get Darktheme => this._darkTheme;
   bool get Customtheme => this._customTheme;
+  ThemeData get CurrentTheme => this._currentTheme;
+
+  ThemeChanger(int theme) {
+    switch (theme) {
+      case 1: //Light
+        _darkTheme = false;
+        _customTheme = false;
+        _currentTheme = ThemeData.light();
+        break;
+      case 2: //Dark
+        _darkTheme = true;
+        _customTheme = false;
+        _currentTheme = ThemeData.dark();
+        break;
+      case 3: //Theme Custom
+        _darkTheme = false;
+        _customTheme = true;
+        break;
+      default:
+        _darkTheme = false;
+        _customTheme = false;
+        _currentTheme = ThemeData.light();
+    }
+  }
 
   set Darktheme(bool value) {
     _customTheme = false;
     _darkTheme = value;
+
+    if (value) {
+      _currentTheme = ThemeData.dark();
+    } else {
+      _currentTheme = ThemeData.light();
+    }
     notifyListeners();
   }
 
   set Customtheme(bool value) {
     _customTheme = value;
     _darkTheme = false;
+    if (value) {
+      _currentTheme = ThemeData.light();
+    } else {
+      _currentTheme = ThemeData.dark();
+    }
     notifyListeners();
   }
 }
